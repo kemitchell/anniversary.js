ccf314aad607c649773a4950f0d7deaa379aba51
Kyle E. Mitchell <kyle@kemitchell.com>
2016-08-23 22:11:27 -0700

    Use JavaScript Standard Style

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var leapDay = require('leap-day')
 var leapYear = require('leap-year')
 
-function anniversary(date, number) {
-  number = ( number === undefined ? 1 : number )
+module.exports = function anniversary (date, number) {
+  number = number === undefined ? 1 : number
   var anniversaryYear = date.getFullYear() + number
   return (
-    ( leapDay(date) && !leapYear(anniversaryYear) ) ?
-      new Date(anniversaryYear, 2, 1) :
-      new Date(anniversaryYear, date.getMonth(), date.getDate()) ) }
-
-module.exports = anniversary
+    (leapDay(date) && !leapYear(anniversaryYear))
+    ? new Date(anniversaryYear, 2, 1)
+    : new Date(anniversaryYear, date.getMonth(), date.getDate())
+  )
+}
